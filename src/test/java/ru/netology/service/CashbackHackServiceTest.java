9ce3d4e847dfe0da6610caa4524c4ed4fecba34e
Shal <kudrja100@mail.ru>
2022-12-12 18:16:36 +0200

    First commit TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void testRemain1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+    @org.junit.Test
+    public void testRemain999(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
+    @org.junit.Test
+    public void testRemain1(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void testRemain0(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected,actual);
+    }
+}
